151b4c092fa518bc7080c4f718c65c7727cb6698
Zixuan Zhao <mike@Zixuans-MacBook-Pro.local>
2016-02-20 03:53:14 -0600

    test

--- db_create.py
+++ db_create.py
@@ -2,19 +2,13 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-from app import app, db
+from app import db
 import os.path
-from migrate.exceptions import DatabaseAlreadyControlledError
 
 db.create_all()
-try:
-    if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-        api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-        api.version_control(SQLALCHEMY_DATABASE_URI,
-                            SQLALCHEMY_MIGRATE_REPO)
-    else:
-        api.version_control(SQLALCHEMY_DATABASE_URI,
-                            SQLALCHEMY_MIGRATE_REPO,
-                            api.version(SQLALCHEMY_MIGRATE_REPO))
-except DatabaseAlreadyControlledError:
-    pass
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
+                        api.version(SQLALCHEMY_MIGRATE_REPO))
