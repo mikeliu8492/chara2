a7d3f0cec36d6b1a494876a4585d3bca4813628b
Zixuan Zhao <mike@Zixuans-MacBook-Pro.local>
2016-02-20 00:36:33 -0600

    test

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ LDAP_PROVIDER_URL = 'ldap://ldap.illinois.edu:389'
 LDAP_BASE_DN = 'OU=People,DC=uiuc,DC=edu'
 if os.environ.get('DATABASE_URL') is None:
    print("database url not found")
-   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+   SQLALCHEMY_DATABASE_URI = 'postgres://xykwnwapszxeof:-1JTJTZmqvM_V8c7mdIcjFfAXT@ec2-107-20-148-211.compute-1.amazonaws.com:5432/d73ip15tss408a' #'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
