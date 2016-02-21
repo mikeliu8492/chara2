4b080faf569af44608304f75051cd4a534d1bc5e
Zixuan Zhao <mike@Zixuans-MacBook-Pro.local>
2016-02-21 00:56:36 -0600

    test

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ SECRET_KEY = 'chara'
 WTF_CSRF_ENABLED = True
 WTF_CSRF_SECRET_KEY = 'csrf-chara'
 
-# os.environ['DATABASE_URL'] = 'postgres://fopafuihjjtpms:fP8IKnMZoTRe74hZO2iqj0S-yF@ec2-54-83-198-111.compute-1.amazonaws.com:5432/dfvi4vsnfolh8h'
-
 LDAP_PROTOCOL_VERSION = 3
 AD_PROVIDER_URL = 'ldap://ad.uillinois.edu:389/'
 AD_BASE_DN = 'cn=%s,OU=People,DC=ad,DC=uillinois,DC=edu'
