6c740872f341178de408ad31b0a96649aafb2d72
Zixuan Zhao <mike@Zixuans-MacBook-Pro.local>
2016-02-21 20:13:57 -0600

    test

--- db_create.py
+++ db_create.py
@@ -8,8 +8,8 @@ import migrate.exceptions
 
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     try:
+        api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
         api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     except migrate.exceptions.DatabaseAlreadyControlledError:
         pass
